aaeecd4b63224fa08440a2a55f0bd80fc70e4ab6
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-11-19 15:17:20 +0100

    -1st

--- src/main/java/com/codecool/marwin1991/controller/UserController.java
+++ src/main/java/com/codecool/marwin1991/controller/UserController.java
@@ -19,6 +19,8 @@ public class UserController {
     @GetMapping("/profile")
     //TODO sprawdzić czy ma role user korzystajać z @PreAuthorize
     public AppUser getCurrentUser(@CurrentUser UserPrincipal userPrincipal){
-        throw new RuntimeException("not implemented");
+
+        return  userRepository.findById(userPrincipal.getId())
+                .orElseThrow(()-> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
 }
--- src/main/java/com/codecool/marwin1991/model/AppUser.java
+++ src/main/java/com/codecool/marwin1991/model/AppUser.java
@@ -1,15 +1,27 @@
 package com.codecool.marwin1991.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class AppUser {
 
-    // TODO this class should be entity with fields
-    //  id; (as String with uuid2 strategy)
-    //  name;
-    //  email;
-    //  imageUrl;
-    //  emailVerified default false;
-    //  password default null;
-    //  AuthProvider provider;
-    //  String providerId;
 
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  private String id;
+  private String name;
+  private String email;
+  private String imageurl;
+  private Boolean emailVerified = false;
+  @JsonIgnore private String password = null;
+  private AuthProvider provider;
+  private String providerId;
 }
